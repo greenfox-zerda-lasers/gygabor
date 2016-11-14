9a2572126b35fd3f092453dbde73d235e16df83f
Gyorgy Gabor <gyorgyg@gmail.com>
2016-11-14 07:58:51 +0100

    week4 day3 hexagon is done

--- week-04/day-3/hexagon.py
+++ week-04/day-3/hexagon.py
@@ -17,26 +17,18 @@ def rec_draw(x, y, size, row):
         return 0
     else:
         for i in range(0, row):
-
             draw_hexa(x + i*(size*1.5), y - z * i, size)
-        time.sleep(0.1)
-        rec_draw(x + i*(size*1.5), y + z *i, size, row - 1)
-            # rec_draw(x + size, y, size)
-            # rec_draw(x + size+size/3, y + p * 2, size)
-            # rec_draw(x + size, y + p * 4, size)
-            # rec_draw(x + size/3, y + p * 4, size)
-            # rec_draw(x, y + p * 2, size)
-            # z = size * 3**0.5/2
+            if i > 0:
+                draw_hexa(x+i*(size*1.5), y - (z * i)+z*2, size)
+                draw_hexa(x+(i*(size*1.5))-(size*1.5), y - (z * i)-z, size)
+            if i > 1:
+                draw_hexa(x+(i*(size*1.5)), y - (z * i)+z*4, size)
+                draw_hexa(x+(i*(size*1.5))-(size*1.5)*2, y - (z * i)-z*2, size)
+            if i > 2:
+                draw_hexa(x+(i*(size*1.5)), y - (z * i)+z*6, size)
+                draw_hexa(x+(i*(size*1.5))-(size*1.5)*3, y - (z * i)-z*3, size)
+
         return rec_draw(x, y, size, row-1)
-
-    #   else:
-    #       for i in range(0, piece):
-    #           p = x
-    #           p += i*size
-    #           draw_triangle(p, y, p + size, y, p + size / 2, y - z)
-      #
-    #       return rec_draw(x+size/2, y-z, size, piece - 1)
-
 
 rec_draw(50, 300, 20, 7)
 
