042dc04e2d86ab2fae08a57fd59ab13fe03ddb31
Gyorgy Gabor <gyorgyg@gmail.com>
2016-11-15 23:25:07 +0100

    cowbull updated

--- week-05/day-1/cowbull/cowbull.py
+++ week-05/day-1/cowbull/cowbull.py
@@ -1,33 +1,76 @@
 import random
 
-class Cab():
-
-
+class Player:
     def __init__(self):
+        self.name = ''
+        self.state = 'playing'
 
-        self.number = random_number()
-        print (self.number)
+class Cab:
 
-    def random_number(self):
-        number = []
-        while len(number) != 4:
+    def __init__(self, player):
+        self.player = player
+        self.number = []
+        self.guess = -1
+        self.guess_number = -1
+        self.game_running = False
+        self.cows = 0
+        self.bulls = 0
+        self.guess_count = 0
+
+        self.start()
+
+    def start(self):
+        self.game_running = True
+        while len(self.number) != 4:
             j = random.randrange(0, 9)
-            if not j in number:
-                number.append(j)
-            return number
+            if not j in self.number:
+                self.number.append(j)
 
-    def which_state(self, result):
-        if result is True:
-            state = 'finished'
-        else:
-            state = 'playing'
-        return state
+        print ( 'What\'s your name?' )
+        self.player_name = input()
+        self.game_loop()
 
-    def count_guess(self):
-        return count += 1
+    def game_loop(self):
+        print (self.number)
+        while self.game_running:
+            if self.number == self.guess_number:
+                print( 'You win!' )
+                print(self.guess_count )
+                self.game_running = False
+                self.state = 'finished'
+                self.guess_count += 1
+                self.restart()
+            else:
+                print('cows: ' + str(self.cows))
+                print('bulls: ' + str(self.bulls))
+
+                self.guess_count += 1
+                self.user_guess()
 
     def user_guess(self):
-        
+        self.guess_number = list(input('Give me a four digit number:'))
+
+        for i in range(len(self.number)):
+            if self.guess_number[i] in self.number:
+                if self.guess_number[i] == self.number[i]:
+                    self.cows += 1
+                else:
+                    self.bulls += 1
 
 
-number = Cab()
+    def restart(self):
+        while not self.game_running:
+            print('restart_question' )
+            answer = str(input('(Y)es or (N)o?'))
+            if answer.lower() == 'y':
+                self.start()
+            elif answer.lower() == 'n':
+                print( 'see_ya' )
+                break # END OF GAME
+            else:
+                print( 'false_yn' )
+
+
+
+player = Player()
+game = Cab(player)
