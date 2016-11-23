fdccecdf0f73a85243ee48ddf539dffef8a4c73d
Gyorgy Gabor <gyorgyg@gmail.com>
2016-11-23 08:13:00 +0100

    tkwanderer: hero is moving, but didn't delete it in the earlier positions

--- week-06/tkwanderer.py
+++ week-06/tkwanderer.py
@@ -7,26 +7,53 @@ class Tkwanderer:
     def __init__(self):
         self.view = view_tkwanderer.Display()
         self.area = model_tkwanderer.Area()
-        self.hero = model_tkwanderer.Hero(32, 32)
+        self.hero = model_tkwanderer.Hero(0, 0)
         self.area_read = []
-
+        self.running = False
+        self.step = 50
         self.start()
 
     def start(self):
+        self.view.canvas.delete("all")
         self.display_area()
-        self.display_hero()
-        self.view.show()
+        self.game()
 
+    def game(self):
+
+        self.display_hero()
+        self.input_handling()
+
+        self.view.show()
     # draw area
     def display_area(self):
         self.area.read_area()
         self.view.draw_area(self.area.game_area)
 
+    # draw hero
     def display_hero(self):
-        self.view.draw_hero(self.hero.posX, self.hero.posY)
+        self.view.draw_hero(self.hero.posX * self.step, self.hero.posY * self.step)
 
-    def game_loop(self):
-        pass
 
+    def input_handling(self):
+        self.view.root.bind('<Up>', self.move_up_hero)
+        self.view.root.bind('<Down>', self.move_down_hero)
+        self.view.root.bind('<Left>', self.move_left_hero)
+        self.view.root.bind('<Right>', self.move_right_hero)
+
+    def move_up_hero(self, event):
+        self.hero.posY -= 1
+        self.start()
+
+    def move_down_hero(self, event):
+        self.hero.posY += 1
+        self.start()
+
+    def move_left_hero(self, event):
+        self.hero.posX -= 1
+        self.start()
+
+    def move_right_hero(self, event):
+        self.hero.posX += 1
+        self.start()
 
 game = Tkwanderer()
--- week-06/view_tkwanderer.py
+++ week-06/view_tkwanderer.py
@@ -6,36 +6,39 @@ class Display:
     def __init__(self):
 
         self.root = Tk()
-        self.canvas = Canvas(self.root, width = 800, height = 800)
+        self.canvas = Canvas(self.root, width = 600, height = 600)
         self.canvas.pack()
 
         # read Area images
-        self.floor = PhotoImage(file = 'floor.gif')
-        self.wall = PhotoImage(file = 'wall.gif')
+        self.floor = ImageTk.PhotoImage(self.image_resize(Image.open('img/floor.png')))
+        self.wall = ImageTk.PhotoImage(self.image_resize(Image.open('img/wall.png')))
 
         # read Character images
-
         # Hero
-        self.hero_right = ImageTk.PhotoImage(Image.open('hero-right.png'))
-        self.hero_down = ImageTk.PhotoImage(Image.open('hero-down.png'))
-        self.hero_left = ImageTk.PhotoImage(Image.open('hero-left.png'))
-        self.hero_up = ImageTk.PhotoImage(Image.open('hero-up.png'))
+        self.hero_right = ImageTk.PhotoImage(self.image_resize(Image.open('img/hero-right.png')))
+        self.hero_down = ImageTk.PhotoImage(self.image_resize(Image.open('img/hero-down.png')))
+        self.hero_left = ImageTk.PhotoImage(self.image_resize(Image.open('img/hero-left.png')))
+        self.hero_up = ImageTk.PhotoImage(self.image_resize(Image.open('img/hero-up.png')))
+
+    def image_resize(self, image):
+        image = image.resize((50, 50), Image.ANTIALIAS)
+        return image
 
     # draw area
     def draw_area(self, area):
 
-        posX = 36
-        posY = 36
+        posX = 25
+        posY = 25
         for i in range(len(area)):
             for j in range(len(area[i])):
                 if area[i][j] == '0':
-                    self.canvas.create_image(posX + (j * 72), posY + (i * 72), image = self.floor)
+                    self.canvas.create_image(posX + (j * 50), posY + (i * 50), image = self.floor)
                 elif area[i][j] == '1':
-                    self.canvas.create_image(posX + (j * 72), posY + (i * 72), image = self.wall)
+                    self.canvas.create_image(posX + (j * 50), posY + (i * 50), image = self.wall)
 
     # draw hero
     def draw_hero(self, posX, posY):
-        self.canvas.create_image(posX, posY, image = self.hero_down)
+        self.canvas.create_image(posX, posY, anchor = NW, image = self.hero_down)
 
     def show(self):
-	       self.root.mainloop()
+	    self.root.mainloop()
