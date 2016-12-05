c9d9db052fb13ec260212d565fc2fdef895900d4
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-05 16:42:13 +0100

    javascript exercises 16-20 are done

--- week-07/day-1/14.js
+++ week-07/day-1/14.js
@@ -1,5 +1,9 @@
 'use strict';
 
-var o = "pneumonoultramicroscopicsilicovolcanoconiosis";
-// tell how many letters o has
-console.log(o.length);
+var n = 'The result is: ';
+var a = 2;
+var b = 3;
+
+// concat the product of a and b to the n string
+n += a + b;
+console.log(n);
