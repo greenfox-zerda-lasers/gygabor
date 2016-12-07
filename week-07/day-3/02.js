c58c3094a7cd18cec582d3b53cbea0d44d135775
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-07 11:12:35 +0100

    day 3, exercise 02 done

--- week-07/day-3/02.js
+++ week-07/day-3/02.js
@@ -3,4 +3,9 @@
 // create a function called apply that takes a function and calls it with one argument
 // that is the string 'apple'
 
+function apply(funct){
+  funct('apply')
+}
+
+
 apply(console.log) // should log apple
