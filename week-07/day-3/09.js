69a81a35caa0ddb602a4941f53b974e76579966f
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-07 15:30:14 +0100

    day 3, exercise 09 done

--- week-07/day-3/09.js
+++ week-07/day-3/09.js
@@ -4,3 +4,18 @@
 // it should return an object thats keys are the letters and the values are
 // the counts.
 // example: "apple" -> {a: 1, p: 2, l: 1, e: 1}
+
+function countLetter(string){
+  var count = {}
+  for (var i = 0; i < string.length; i++){
+    var character = string.charAt(i);
+      if (count[character]) {
+        count[character]++;
+      } else {
+        count[character] = 1;
+      }
+  }
+  return count
+}
+
+console.log(countLetter('apple'));
