1645c84523aefdba62ff7cf26d55f4edc7f2bfac
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-07 12:05:23 +0100

    day 3, exercise 05 done

--- week-07/day-3/05.js
+++ week-07/day-3/05.js
@@ -6,3 +6,13 @@ var numbers = [2.4, 3.5, 1.7, 3.3, 1.2];
 // it should return a new array that consists only the numbers that are
 // bigger than 2 and all of it's elements should be rounded
 
+function biggerTwo(numberArray){
+  var biggerNumber = numberArray.filter(function (num){
+    return num > 2;
+  }).map(function(num){
+    return Math.round(num);
+  });
+  return biggerNumber;
+}
+
+console.log(biggerTwo(numbers));
