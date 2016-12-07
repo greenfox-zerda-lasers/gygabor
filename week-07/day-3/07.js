f97cc1b29f58f48696b9d928e5c1d47d6130dafe
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-07 14:42:00 +0100

    day 3, exercise 07 done

--- week-07/day-3/07.js
+++ week-07/day-3/07.js
@@ -1,6 +1,20 @@
 'use strict';
 
-var numbers = [2, 5, 11, 29];
+var numbers = [2, 3, 5, 7, 8];
 
 // create a function that takes an array of numbers and returns a boolean
 // it should return true if all the elements are prime, false otherwise
+
+function isPrime(numbers){
+  return numbers.every(function(num){
+    for (var i = 2; i <= Math.floor(Math.sqrt(num)); i++){
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  });
+
+}
+
+console.log(isPrime(numbers));
