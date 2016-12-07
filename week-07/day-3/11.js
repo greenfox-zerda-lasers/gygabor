f059debce07d11b10b9a4f4edde38cd8eee9e1b2
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-07 17:10:59 +0100

    day 3, exercise 11 done

--- week-07/day-3/11.js
+++ week-07/day-3/11.js
@@ -6,3 +6,28 @@
 // It should have a `pop` method that returns the last element form the stack and also deletes it from it
 
 // please don`t use the built in methods
+
+function Stack(element){
+  var elements = element;
+  this.size = function(){
+    return elements.length;
+
+  };
+  this.push = function(elem) {
+    elements[elements.length] = elem;
+  };
+  this.pop = function(){
+    var lastElem = elements[elements.length - 1]
+    elements.length--
+    return lastElem
+  }
+}
+
+var stack = new Stack([3,4,4,4])
+console.log(stack.size())
+
+stack.push(1)
+console.log(stack.size())
+
+console.log(stack.pop())
+console.log(stack.size())
