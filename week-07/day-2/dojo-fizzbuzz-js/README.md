a88d994ca3d149f07059ee9a48185d6343677929
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-07 10:48:16 +0100

    week7 day2, day3: exercises downloaded

---
+++ week-07/day-2/dojo-fizzbuzz-js/README.md
@@ -0,0 +1,1 @@
+# dojo-fizzbuzz-js
---
+++ week-07/day-2/dojo-fizzbuzz-js/test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var test = require('tape');
+var index = require('./index');
+
+test('true', function(t) {
+  t.equal(true, true);
+  t.end();
+});
