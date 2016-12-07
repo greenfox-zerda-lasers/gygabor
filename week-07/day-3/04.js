efe01debe236796d67cbde1b49350ef9f3538879
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-07 11:43:25 +0100

    day 3, exercise 04 done

--- week-07/day-3/04.js
+++ week-07/day-3/04.js
@@ -2,3 +2,11 @@
 
 var af = [4, 5, 6, 7];
 // print all the elements of af, dont use for or while :)
+
+function printAll(arr){
+  arr.forEach(function(element){
+    console.log(element);
+  });
+}
+
+printAll(af);
