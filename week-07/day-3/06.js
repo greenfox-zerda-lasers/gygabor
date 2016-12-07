9653c1de92ad318fa5e4aa997378dc4229ed9b92
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-07 12:21:57 +0100

    day 3, exercise 06 done

--- week-07/day-3/06.js
+++ week-07/day-3/06.js
@@ -3,3 +3,9 @@
 
 // create a function that takes a string and a letter and returns a boolean
 // it should return true if the string consits the given letter, false otherwise
+
+function letterInString(string, letter){
+  return string.indexOf(letter) === 0;
+}
+
+console.log(letterInString('abc', 'd'))
