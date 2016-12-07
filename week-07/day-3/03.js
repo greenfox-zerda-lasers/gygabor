3d90d3386c93980c009b7d0889aa4220a377d3dd
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-07 11:29:06 +0100

    day exercise3: done

--- week-07/day-3/03.js
+++ week-07/day-3/03.js
@@ -3,3 +3,12 @@
 // write a function called each that takes an array and a function as a paramter
 // and calls the function with each element of the array as parameter
 // so it should call the array 3 times if the array has 3 elements
+var elements = [1, 2, 3]
+
+function each(arr, funct){
+  arr.forEach(function(element, index, array){
+    funct(element, array)
+  });
+}
+
+each(elements, console.log)
