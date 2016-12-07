d677baccf4ec07219faeae1b365da2e28392cbc5
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-07 16:21:08 +0100

    day 3, exercise 10 done

--- week-07/day-3/10.js
+++ week-07/day-3/10.js
@@ -3,3 +3,25 @@
 // create a student object
 // that has a method `addgrade`, that takes a grade from 1 to 5
 // an other method `getAverage`, that returns the average of the grades
+
+var student = {
+  grades : [],
+  addgrade : function(num){
+    if (num >= 1 && num <= 5){
+      this.grades.push(num);
+    }
+  },
+  getAverage : function(gradeList){
+    var sum = 0;
+    for (var i = 0; i < gradeList.length; i++){
+      sum += gradeList[i];
+    }
+    return sum / gradeList.length;
+  }
+}
+
+student.addgrade(4);
+student.addgrade(5);
+student.addgrade(1);
+student.addgrade(2);
+console.log(student.getAverage(student.grades), student.grades)
