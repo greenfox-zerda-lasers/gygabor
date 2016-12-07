b90e1a3b51378fece14c81d584d9b628ed7551e5
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-07 14:49:54 +0100

    day 3, exercise 08 done

--- week-07/day-3/08.js
+++ week-07/day-3/08.js
@@ -6,10 +6,21 @@ var students = [
   {name: 'Aurel', age: 7, candies: 3},
   {name: 'Zsombor', age: 12, candies: 5},
   {name: 'Olaf', age: 12, candies: 7},
-  {name: 'Teodor', age: 3, candies: 2}
+  {name: 'Teodor', age: 3, candies: 8}
 ];
 
 
 // create a function that counts the students that
 // has more than 4 candies
 
+function moreFour(studentList){
+  var sum = 0;
+  for (var i = 0; i < studentList.length; i++){
+    if (students[i].candies > 4){
+      sum++;
+    }
+  }
+  return sum;
+}
+
+console.log(moreFour(students));
