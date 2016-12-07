c35c8e0a9989e086d8aa855b3dfacbd577458d77
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-07 11:06:19 +0100

    day 3, exercise 01 done

--- week-07/day-3/01.js
+++ week-07/day-3/01.js
@@ -3,3 +3,13 @@
 // Create a function that takes a number and returns it as string in english
 // like 0 -> "zero", it should work with the first 5 numbers, after it should
 // return "many"
+
+var numberName = ['zero', 'one', 'two', 'three', 'four', 'five'];
+
+function stringBack(number){
+  if (number in numberName){
+    return numberName[number];
+  }
+}
+
+console.log(stringBack(0))
