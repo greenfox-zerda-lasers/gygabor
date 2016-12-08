1e24234b9c9efd6b152c2dde323045a86aeee978
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-08 13:29:02 +0100

    content-2.html exercise is modified

--- week-07/day-4/content-2.js
+++ week-07/day-4/content-2.js
@@ -1,8 +1,18 @@
 // fill every paragraph with the last one's content.
-var lastPar = document.getElementsByClassName('dog');
+
+// var lastPar = document.getElementsByClassName('dog');
+//
+// var allPar = document.getElementsByTagName('p');
+//
+// for (var i = 0; i < allPar.length; i++){
+//   allPar[i].innerHTML = lastPar[0].innerHTML
+// }
 
 var allPar = document.getElementsByTagName('p');
 
+var lastIndex = allPar.length - 1
+console.log(lastIndex)
+
 for (var i = 0; i < allPar.length; i++){
-  allPar[i].innerHTML = lastPar[0].innerHTML
+  allPar[i].innerHTML = allPar[lastIndex].innerHTML
 }
