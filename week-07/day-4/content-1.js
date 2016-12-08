51774b9f970a1a7904c1ab58b2249093f4da280f
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-08 13:12:41 +0100

    content-1.html exercise is modified

--- week-07/day-4/content-1.js
+++ week-07/day-4/content-1.js
@@ -1,7 +1,10 @@
 // 1. Alert the content of the heading.
 var head = document.getElementsByTagName('h1');
-alert(head[0].innerText);
-
+if (head){
+  for (var i = 0; i < head.length; i++){
+    alert(head[i].innerText);
+  }
+}
 // 2. Write the content of the first paragraph to the console.
 var firstParagraph = document.querySelector('p');
 console.log(firstParagraph);
@@ -11,7 +14,11 @@ var secondParagraph = document.getElementsByClassName('other');
 console.log(secondParagraph);
 
 // 4. Replace the heading's content with 'New content'.
-head[0].innerHTML = 'New content'
+if (head){
+  for (var i = 0; i < head.length; i++){
+    head[0].innerHTML = 'New content';
+  }
+}
 
 // 5. Replace the last paragraph's content with the first paragraph's content.
 var thirdParagraph = document.getElementsByClassName('result');
