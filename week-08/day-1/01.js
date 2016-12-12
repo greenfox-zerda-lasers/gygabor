ddb0009942c5336b9d0d143b06b050399ab8f03f
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-12 11:38:07 +0100

    week-08/day-1, 1.js: done

---
+++ week-08/day-1/01.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Create a constructor for creating Animals.
+// it should take one parameter: what the animal says
+// Every animal should have a method called say() that prints what the animal says
+
+function Animals(talk){
+  this.talk = talk;
+}
+
+Animals.prototype.say = function() {
+  console.log(this.talk);
+}
+
+var frog = new Animals('Brek');
+frog.say();
