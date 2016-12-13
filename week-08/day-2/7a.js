cff32470e249c4439d40854b640bdd8f3543ca2b
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-13 12:20:04 +0100

    week08/day-2/07a.js: done

---
+++ week-08/day-2/7a.js
@@ -0,0 +1,15 @@
+// Embed the following file to a HTML document:
+//  - https://www.flickr.com/photos/130463184@N04/15584243094 (use the embed feature)
+//  - watch the DOMContentLoaded event also and change the background color of the page to red while the image is loading
+//  - add a 'load' event to the window object that logs "loaded" when the image is downloaded and changes back the background color of the page to white
+
+var body = document.getElementsByTagName('body')
+
+document.addEventListener('DOMContentLoaded', function(){
+  body[0].style.backgroundColor = "red";
+});
+
+window.addEventListener('load', function(){
+  body[0].style.backgroundColor = "white";
+  console.log('loaded');
+});
