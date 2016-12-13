1d7eec08f7eacff6deb1af3ff7d32e20293f4f15
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-13 10:54:54 +0100

    week08/day-2/04.js: done

---
+++ week-08/day-2/04.js
@@ -0,0 +1,18 @@
+// set up a setInterval loop with 1.5 second delays
+// - log the mouse coordinates on each call
+
+var cursorX;
+var cursorY;
+
+document.onmousemove = function(e){
+    cursorX = e.pageX;
+    cursorY = e.pageY;
+}
+
+function checkCursor(){
+    console.log("Cursor at: " + cursorX + ", " + cursorY);
+}
+
+while (true){
+  setTimeout(checkCursor, 1500);
+}
