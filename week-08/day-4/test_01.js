cdf3a83f54f846b839b8ffc18cfcd2cdf5b2f32a
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-15 11:33:50 +0100

    Week08/day04 1.jp: modified 2.js: created

--- week-08/day-4/test_01.js
+++ week-08/day-4/test_01.js
@@ -14,6 +14,11 @@ test('test double() gives back doublednumber', function(t){
 });
 
 test('test double() gives back doublednumber', function(t){
+  t.notEqual(double(1), 1);
+  t.end();
+});
+
+test('test double() gives back doublednumber', function(t){
   t.equal(double('1'), 2);
   t.end();
 });
