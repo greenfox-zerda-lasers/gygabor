c4d30400c19c01aad74eadf74e8c63d90c5788a4
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-19 13:57:29 +0100

    nodejs 01 exercise modified

--- week-09/day-1/01.js
+++ week-09/day-1/01.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function a(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('The request come form: ' + req.url + '\nHey! Fuuuu: ' + new Date().toISOString());
+  res.end('The request come form: ' + req.url + '\nHey! Fuuuu: ' + new Date());
 });
 
 server.listen(3000, '127.0.0.1');
