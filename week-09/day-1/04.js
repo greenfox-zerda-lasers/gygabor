8101dd238ab072f95cc6f091e30a9338148f9ea2
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-19 15:45:39 +0100

    nodejs 4 exercise modified

--- week-09/day-1/04.js
+++ week-09/day-1/04.js
@@ -5,6 +5,10 @@ var express = require('express');
 
 var app = express();
 
+app.get('/todos', function a(req, res) {
+  res.send(jason);
+});
+
 app.get('/todos/:id', function a(req, res) {
   res.send(jason.filter(function (e){
     return parseInt(req.params.id, 10) === e['id'];
