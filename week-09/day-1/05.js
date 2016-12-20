e145fdba2724f8bd31dcb417f0ab3a7ec2ed395f
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-20 11:44:40 +0100

    node js05 modified

--- week-09/day-1/05.js
+++ week-09/day-1/05.js
@@ -16,12 +16,11 @@ app.get('/todos', function a(req, res) {
 
 app.get('/todos/:id', function a(req, res) {
   res.send(jason.filter(function (e){
-    return parseInt(req.params.id, 10) === e['id'];
+    return parseInt(req.params.id, 10) === e.id;
   }));
 });
 
 app.post('/todos', urlencodedParser, function a(req, res) {
-  console.log(req.body);
   var todo = {
     completed: Boolean(req.body.completed),
     id: jason.length + 1,
@@ -29,6 +28,20 @@ app.post('/todos', urlencodedParser, function a(req, res) {
   };
   jason.push(todo);
   res.send(JSON.stringify(todo));
+});
+
+app.put('/todos/:id', urlencodedParser, function a(req, res) {
+  var todo = (jason.filter(function (e){
+    return parseInt(req.params.id, 10) === e.id;
+  }));
+  todo = {
+    completed: Boolean(req.body.completed),
+    id: parseInt(req.params.id),
+    text: req.body.text,
+  };
+  console.log(todo);
+  jason.push(todo);
+  res.send(JSON.stringify(todo))
 });
 
 app.listen(3000);
