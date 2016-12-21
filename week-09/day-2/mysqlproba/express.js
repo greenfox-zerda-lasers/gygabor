e10fc3aab585d585ade13a600e8c6e8bada18eb5
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-21 15:31:16 +0100

    node mysql todo 'get' done

--- week-09/day-2/mysqlproba/express.js
+++ week-09/day-2/mysqlproba/express.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'cookies',
-  database: 'bookstore'
+  database: 'todo'
 });
 
 connection.connect(function (error){
@@ -20,18 +20,8 @@ connection.connect(function (error){
   }
 });
 
-// app.get('/', function(req, res) {
-//    connection.query('SELECT book_name FROM book_mast;',function(err,rows){
-//      if(err) {
-//        console.log(err.toString());
-//        return;
-//      }
-//      res.send(rows);
-//    });
-//  });
-
 app.get('/', function(req, res) {
-   connection.query('SELECT book_name, aut_name, cate_descrip, book_price pub_name FROM book_mast INNER JOIN author ON book_mast.aut_id = author.aut_id INNER JOIN category ON book_mast.cate_id = category.cate_id INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id;',function(err,rows){
+   connection.query('SELECT * FROM todo',function(err,rows){
      if(err) {
        console.log(err.toString());
        return;
