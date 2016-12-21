9179ff3a2f96656c565602157b0da753e7d8c6ac
Gyorgy Gabor <gyorgyg@gmail.com>
2016-12-21 12:23:50 +0100

    node mysql 02 exercise done

--- week-09/day-2/mysqlproba/index.js
+++ week-09/day-2/mysqlproba/index.js
@@ -15,12 +15,27 @@ connection.connect(function (error){
   }
 });
 
-connection.query("SELECT book_name FROM book_mast;", function (err,rows) {
-  console.log("Data received from Db:\n");
-  console.log(rows);
-});
+// connection.query("SELECT book_name FROM book_mast;", function (err,rows) {
+//   console.log("Data received from Db:\n");
+//   console.log(rows);
+// });
+//
+// connection.query("SELECT book_name, aut_name FROM book_mast JOIN author ON book_mast.aut_id = author.aut_id;", function (err,rows) {
+//   console.log("Data received from Db:\n");
+//   console.log(rows);
+// });
 
-connection.query("SELECT book_name, aut_name FROM book_mast JOIN author ON book_mast.aut_id = author.aut_id;", function (err,rows) {
+// connection.query("SELECT book_name, aut_name, cate_descrip FROM book_mast JOIN author ON book_mast.aut_id = author.aut_id JOIN category ON book_mast.cate_id = category.cate_id;", function (err,rows) {
+//   console.log("Data received from Db:\n");
+//   console.log(rows);
+// });
+
+// connection.query("SELECT book_name, aut_name, cate_descrip, pub_name FROM book_mast INNER JOIN author ON book_mast.aut_id = author.aut_id INNER JOIN category ON book_mast.cate_id = category.cate_id INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id;", function (err,rows) {
+//   console.log("Data received from Db:\n");
+//   console.log(rows);
+// });
+
+connection.query("SELECT book_name, aut_name, cate_descrip, book_price pub_name FROM book_mast INNER JOIN author ON book_mast.aut_id = author.aut_id INNER JOIN category ON book_mast.cate_id = category.cate_id INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id;", function (err,rows) {
   console.log("Data received from Db:\n");
   console.log(rows);
 });
