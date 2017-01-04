3fd7efeac575273b1aafa7a9b80e801f48b4d7a3
Gyorgy Gabor <gyorgyg@gmail.com>
2017-01-04 19:17:11 +0100

    Audioplayer: some kind of server.js is exists

--- week-10/audioplayer/server.js
+++ week-10/audioplayer/server.js
@@ -7,11 +7,20 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.json());
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+//   next();
+// });
+
+
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'cookies',
-  database: 'todo'
+  database: 'foxplayer'
 });
 
 connection.connect(function (error){
@@ -22,3 +31,54 @@ connection.connect(function (error){
     console.log ('WOOOOWW');
   }
 });
+
+app.get('/playlists', function(req, res) {
+	connection.query('SELECT * FROM playlists', function(err, rows, fields) {
+		if (err) throw err;
+  		res.send(rows);
+	});
+});
+
+app.post('/playlists', function (req, res) {
+  connection.query('INSERT INTO playlists (playlist, system) VALUES ("' + req.body.playlist + '", 0);', function(err, rows, fields) {
+		if (err) throw err;
+  		res.send(rows);
+	});
+});
+
+app.delete('/playlists/:id', function (req, res) {
+  connection.query('DELETE FROM playlists WHERE id = \'' + req.params.id + '\';', function(err, rows, fields) {
+		if (err) throw err;
+  		res.send(rows);
+	});
+}),
+
+app.get('/playlist-tracks', function (req, res) {
+  connection.query('SELECT * FROM tracks', function(err, rows, fields) {
+		if (err) throw err;
+  		res.send(rows);
+	});
+}),
+
+app.get('/playlist-tracks/:playlist_id', function (req, res) {
+  connection.query('SELECT * FROM tracks WHERE playlist_id = \'' + req.params.playlist_id + '\';', function(err, rows, fields) {
+		if (err) throw err;
+  		res.send(rows);
+	});
+}),
+
+app.post('/playlist-tracks', function (req, res) {
+  connection.query('INSERT INTO tracks (path, playlist_id) VALUES ("' + req.body.path + '", 0);', function(err, rows, fields) {
+		if (err) throw err;
+  		res.send(rows);
+	});
+}),
+
+app.post('/playlists-tracks/:playlist_id', function (req, res) {
+  connection.query('UPDATE tracks SET playlist_id = "' + req.params.playlist_id + '";', function(err, rows, fields) {
+		if (err) throw err;
+  		res.send(rows);
+	});
+}),
+
+app.listen(3000);
