1297ed2dba4f6168187f37958e046e5bda887cdd
Gyorgy Gabor <gyorgyg@gmail.com>
2017-03-28 00:49:39 +0200

    fix(pip-io): fix some minor mistake

--- week-04/day-1/02_pip/lottery/lottery.py
+++ week-04/day-1/02_pip/lottery/lottery.py
@@ -6,8 +6,8 @@ def five_most_frequent():
     lottery_data = lottery.read()
     lottery_data = lottery_data.split('\n')
     repository = []
-    result = {1:[0,0], 2:[0,0], 3:[0,0], 4:[0,0], 5:[0,0]}
-    counted_number= 0
+    result = {1:[0, 0], 2:[0, 0], 3:[0, 0], 4:[0, 0], 5:[0, 0]}
+    counted_number = 0
     j = 1
 
     for i in lottery_data:
@@ -19,7 +19,7 @@ def five_most_frequent():
         i = str(i)
         j = 1
         counted_number = repository.count(i)
-        print (counted_number)
+        print(counted_number)
         while j <= 5:
             if counted_number > result[j][1]:
                 result[j] = [i, counted_number]
@@ -29,14 +29,14 @@ def five_most_frequent():
 
     table = PrettyTable(['number', 'frequency'])
     # table.padding_width = 1 # One space between column edges and contents (default)
-    table.add_row([result[1][0],result[1][1]])
-    table.add_row([result[2][0],result[2][1]])
-    table.add_row([result[3][0],result[3][1]])
-    table.add_row([result[4][0],result[4][1]])
-    table.add_row([result[5][0],result[5][1]])
+    table.add_row([result[1][0], result[1][1]])
+    table.add_row([result[2][0], result[2][1]])
+    table.add_row([result[3][0], result[3][1]])
+    table.add_row([result[4][0], result[4][1]])
+    table.add_row([result[5][0], result[5][1]])
 
     print(table)
-    return (table)
     lottery.close()
+    return table
 
 five_most_frequent()
--- week-04/day-3/1.py
+++ week-04/day-3/1.py
@@ -1,3 +1,4 @@
+# pylint: disable=missing-docstring
 # create a 300x300 black canvas.
 # draw a red horizontal line to its middle.
 # draw a green vertical line to its middle.
@@ -5,9 +6,9 @@
 from tkinter import *
 root = Tk()
 
-canvas = Canvas(root, width = 300, height = 300, background = 'black')
-horizontal_line = canvas.create_line(0, 150, 300, 150, fill = 'red')
-vertical_line = canvas.create_line(150, 0, 150, 300, fill= 'green')
+canvas = Canvas(root, width=300, height=300)
+h_line = canvas.create_line(0, 150, 300, 150, fill = 'red')
+v_line = canvas.create_line(150, 0, 150, 300, fill= 'green')
 canvas.pack()
 
 root.mainloop()
